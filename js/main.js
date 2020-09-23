ca856fcbcc3d399feb3889a3e08a52c7b7bdd048
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-23 19:02:33 +0530

    Add project section

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+    let $btns = $('.project-area .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.project-area .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.project-area .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+
+});
