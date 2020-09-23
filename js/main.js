160318a3c5b55e6b1338cfd6eb1996d75465feec
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-23 20:10:01 +0530

    Add recommendation with owl carousel

--- js/main.js
+++ js/main.js
@@ -24,4 +24,20 @@ $(document).ready(function () {
         gallery: { enabled: true }
     });
 
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+
 });
