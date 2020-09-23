423a8817f0a382e1e0233526eb0ab3ef00cfb3dd
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-23 21:32:02 +0530

    Made responsive

--- js/main.js
+++ js/main.js
@@ -40,4 +40,23 @@ $(document).ready(function () {
         }
     })
 
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
 });
