7dc16b69c3b44c51dcc626894ae412cced176f34
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-23 19:18:38 +0530

    Add magnific-popup functionality on project image gallery

--- js/main.js
+++ js/main.js
@@ -19,5 +19,9 @@ $(document).ready(function () {
 
     $('.project-area .button-group #btn1').trigger('click');
 
+    $('.project-area .grid .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
 
 });
