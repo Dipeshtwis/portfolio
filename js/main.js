549c27954fa67b4decffe606cd334bc98096a65f
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-18 19:05:25 +0530

    Add progress bar

--- js/main.js
+++ js/main.js
@@ -60,3 +60,14 @@ $(document).ready(function () {
     navbarFixed();
 
 });
+
+      // progress bar
+
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+    }
